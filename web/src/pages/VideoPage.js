cf40dd1f5fadb01255226fc3b5f43e523e0cebba
Ronit <ronitcodes@gmail.com>
2025-08-12 21:10:26 -0700

    Add VideoPage component

---
+++ web/src/pages/VideoPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function VideoPage() {
+  // List videos, play selected, select subtitle language
+  return (
+    <div>
+      <h2>Offline Videos</h2>
+      {/* Video list and player UI here */}
+    </div>
+  );
+}
